070ba12cb05f315e8caf704f7ebd86701cd502d9
Kristin Beltz <beltz08@me.com>
2015-09-01 13:54:23 -0500

    Update README. Generate Page model, view, controller. Move root route to pages#index.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901150650) do
+ActiveRecord::Schema.define(version: 20150901184504) do
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.date     "date"
