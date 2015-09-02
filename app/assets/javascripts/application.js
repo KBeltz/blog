fea0de0cb78072439bec4a95f22a4d9e366ad1ea
Kristin Beltz <beltz08@me.com>
2015-09-02 11:56:08 -0500

    Fixes and Clean Ups
    Fix Unicorn button on pages#about and removed extraneous jQuery.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,10 +31,3 @@
 //= require js/foundation/foundation.topbar.js
 //= require turbolinks
 //= require_tree .
-
-jQuery(document).ready(function($) {
-  $(".scroll").click(function(event) {
-  event.preventDefault();
-  $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);
-  } );
-} );
