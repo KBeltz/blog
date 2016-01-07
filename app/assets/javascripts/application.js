542fe006d4beffd218a95c1289d0403638a3328b
Kristin Beltz <beltz08@me.com>
2016-01-06 23:14:26 -0600

    Add javascript toggle function for about page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,9 @@
 //= require js/foundation/foundation.topbar.js
 //= require turbolinks
 //= require_tree .
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if (e.style.display == 'block' || e.style.display=='') e.style.display = 'none';
+    else e.style.display = 'block';
+}
