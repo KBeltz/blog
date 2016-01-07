aed512ee87e7c2dc3d8419c6fdc6ad8b6ab027b1
Kristin Beltz <beltz08@me.com>
2016-01-07 00:20:59 -0600

    Edit toggle functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,8 +32,12 @@
 //= require turbolinks
 //= require_tree .
 
-function toggle_visibility(id) {
-    var e = document.getElementById(id);
-    if (e.style.display == 'block' || e.style.display=='') e.style.display = 'none';
-    else e.style.display = 'block';
+function hideMe(id) {
+  var a = document.getElementById(id);
+  a.style.display = 'none';
+}
+
+function showMe(id) {
+  var b = document.getElementById(id);
+  b.style.display = 'block';
 }
